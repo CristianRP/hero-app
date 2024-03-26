73a54b9f2497fc1e44dc487c2880c6da22491081
Cristian Ramirez <cristianramirezgt@gmail.com>
2024-03-26 13:17:42 -0600

    feat: Add styling to HeroPage

--- src/heroes/pages/HeroPage.tsx
+++ src/heroes/pages/HeroPage.tsx
@@ -1,9 +1,10 @@
-import { Navigate, useParams } from 'react-router-dom'
+import { Navigate, useNavigate, useParams } from 'react-router-dom'
 import { getHeroById } from '../helpers';
 
 export const HeroPage = () => {
 
   const { id } = useParams();
+  const navigate = useNavigate();
 
   if (!id) {
     return <Navigate to={'/'} />
@@ -15,9 +16,40 @@ export const HeroPage = () => {
     return <Navigate to="/" replace={true} />
   }
 
+  const onNavigateBack = () => {
+    navigate(-1);
+  }
+
   return (
-    <>
-      <h1>{ hero.superhero }</h1>
-    </>
+    <div className='flex flex-wrap mt-5'>
+      <div className="w-full md:w-1/2 lg:w-1/3 xl:w-1/4">
+        <img src={`/assets/heroes/${ id }.jpg`} alt={ hero.superhero } className='rounded-lg' />
+      </div>
+
+      <div className="w-full md:w-1/2 lg:w-2/3 xl:w-3/4 px-4">
+        <h3 className='text-2xl font-bold'>{ hero.superhero }</h3>
+        <ul className="list-none">
+          <li>
+            <b>Alter ego:</b>{ hero.alter_ego }
+          </li>
+          <li>
+            <b>Publisher:</b>{ hero.publisher }
+          </li>
+          <li>
+            <b>First Appearance:</b>{ hero.first_appearance }
+          </li>
+        </ul>
+
+        <h5 className="mt-3 text-lg font-semibold">Characters</h5>
+        <p>{ hero.characters }</p>
+
+        <button
+          className='bg-blue-400 rounded-md p-2 text-white mt-5'
+          onClick={ onNavigateBack }
+          >
+          Back
+        </button>
+      </div>
+    </div>
   )
 }
